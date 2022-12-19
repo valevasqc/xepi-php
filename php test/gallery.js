f01f3cf315089f9ef435ba2f05c40844fd97a2fe
LCT-Dev <121063608+LCT-Dev@users.noreply.github.com>
2022-12-19 17:27:08 -0600

    reemplazar DOM con Click

--- php test/gallery.js
+++ php test/gallery.js
@@ -1,7 +1,8 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("click", () => {
   // (A) GET ALL IMAGES
-  var all = document.querySelectorAll(".gallery img");
   
+  var all = document.querySelectorAll(".gallery img");
+  console.log(all);
   // (B) CLICK ON IMAGE TO TOGGLE FULLSCREEN
   if (all.length>0) { for (let img of all) {
     img.onclick = () => { img.classList.toggle("full"); };
